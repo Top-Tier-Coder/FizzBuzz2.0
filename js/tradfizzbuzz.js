44af6055ffd3dc8beb5bba4238104004cd8d4fc5
Dan <m.d.omalley@outlook.com>
2021-01-13 08:58:26 -0500

    Commit to Master

---
+++ js/tradfizzbuzz.js
@@ -0,0 +1,18 @@
+function fizzBuzzA(value1, value2){
+    let returnValue = "";
+    for(let i = 1;i<=100;1++){
+        if(i%value1==0 && i%value2==0){
+            returnValue += 'FizzBuzz ';
+        }
+        else if (ivalue1 ==0){
+            returnValue += 'Fizz ';
+        }
+        else if (ivalue2 == 0){
+            returnValue += 'Buzz ';
+        }
+        else{
+            returnValue += i + ' ';
+        }
+    }
+    return returnValue;
+}
