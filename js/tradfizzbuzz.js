e1c02f664c906471e40fc3c3e74ddf235cb8fc05
Dan <m.d.omalley@outlook.com>
2021-01-14 10:13:53 -0500

    master

--- js/tradfizzbuzz.js
+++ js/tradfizzbuzz.js
@@ -1,13 +1,14 @@
-function fizzBuzzA(value1, value2){
+
+function FizzBuzzA(value1, value2){
     let returnValue = "";
-    for(let i = 1;i<=100;1++){
+    for(let i = 1;i<=100;i++){
         if(i%value1==0 && i%value2==0){
             returnValue += 'FizzBuzz ';
         }
-        else if (ivalue1 ==0){
+        else if (i%value1 == 0){
             returnValue += 'Fizz ';
         }
-        else if (ivalue2 == 0){
+        else if (i%value2 == 0){
             returnValue += 'Buzz ';
         }
         else{
@@ -17,3 +18,88 @@ function fizzBuzzA(value1, value2){
     return returnValue;
 }
 
+function FizzBuzzB(value1, value2){
+    let returnValue = "";
+    let Fizz = false;
+    let Buzz = false;
+    for(let i = 1;i<=100;i++){
+        Fizz = i%value1 == 0;
+        Buzz = i%value2 == 0;
+        switch(true){
+            case Fizz && Buzz :{
+                returnValue += 'FizzBuzz ';
+                break;
+            }
+            case Fizz :{
+                returnValue += 'Fizz ';
+                break;
+            }
+            case Buzz :{
+                returnValue += 'Buzz ';
+                break;
+            }
+            default : {
+                returnValue +=i + ' ';
+                break;
+            }
+        }
+    }
+    return returnValue;
+}
+// Non-Traditional Approach
+function FizzBuzzC(value1,value2){
+    let returnValue = "";
+    for (let i =1;i<100;i++){
+        returnValue += ((i%value1==0 ? 'Fizz' : '') + (i%value2==0 ? 'Buzz' : '') || i) + ' ';
+    }
+    return returnValue;
+}
+
+//FizzBuzz the Meta Strat
+function FizzBuzzD(value1, value2){
+    let returnArray= [];
+    for(let i = 1; i<=100;++i){
+        returnArray[i] = ((i%value1==0 ? 'Fizz' : '') + (i%value2==0 ? 'Buzz' : '') || i); 
+    }
+    return returnArray;
+}
+
+// Call and Use Our FizzBuzz
+function buzzIt(){
+    let output="";
+    let val1 = document.getElementById("Fizzvalue").value;
+    let val2 = document.getElementById("Buzzvalue").value;
+    output = FizzBuzzC(val1,val2);
+    document.getElementById("results").innerHTML = output;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =====Solution 2=========
+
+
+// for (var i = 1; i <= 100; i++) {
+
+//     var output = "";
+
+//     if( i % 3 == 0 ) { output += "Fizz"; }
+//     if( i % 5 == 0 ) { output += "Buzz"; }
+
+//     if(output == "") {output = i; }
+//         return output
+
+// }
